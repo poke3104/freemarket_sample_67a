8837f831f43526e5bee5b581dd6c27d0c23a218b
akkun016 <nakataakito@icloud.com>
2020-04-25 20:28:03 +0900

    Revert "Merge pull request #11 from maru1124/createConfirmationPage-front"
    This reverts commit bc7194a02943f06a2a7fa681dd0f8681ab5e4705, reversing
    changes made to b20ddc4d26ac4b26f46218cad0fc490efdeba0c3.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root "tops#index"
 
   resources :detail_pages, only: [:index]
-  resources :confirmation_pages, only: [:index]
 end
--- spec/requests/confirmation_pages_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "ConfirmationPages", type: :request do
-
-end
