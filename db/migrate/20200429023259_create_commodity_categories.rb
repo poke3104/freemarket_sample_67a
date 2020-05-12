62fd1cdda019b8b135ef6bbc0fc96fec180c257e
akkun016 <nakataakito@icloud.com>
2020-05-12 10:36:44 +0900

    Revert "fix"
    This reverts commit e15ee1dc9dd8acd435fed0f816dcd854ef8e402d.

--- db/migrate/20200429023259_create_commodity_categories.rb
+++ db/migrate/20200429023259_create_commodity_categories.rb
@@ -1,8 +1,6 @@
 class CreateCommodityCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :commodity_categories do |t|
-      t.references :commodity, null: false, forein_key: true
-      t.references :category,  null: false, forein_key: true
       t.timestamps
     end
   end
