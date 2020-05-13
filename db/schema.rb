f99beae81b1a82a8b7e4727c8c0ed005d754036c
akkun016 <nakataakito@icloud.com>
2020-05-13 14:21:13 +0900

    ancestryの導入、seed.rbの記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_112522) do
+ActiveRecord::Schema.define(version: 2020_05_12_015551) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_number", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_05_08_112522) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
