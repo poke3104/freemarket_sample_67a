7198471323039b9317f2b354f4c97e7d73d34d2d
akkun016 <nakataakito@icloud.com>
2020-05-24 18:50:24 +0900

    Revert "Revert "Merge branch 'master' into detail-page-server""
    This reverts commit 6a42e0af7ebab5a249ae48d5e66e3f58968c6e06.

--- app/controllers/credit_controller.rb
+++ app/controllers/credit_controller.rb
@@ -1,5 +1,54 @@
 class CreditController < ApplicationController
-  def index
-    
+  require "payjp"
+  protect_from_forgery
+  before_action :set_card
+
+  def new
+    redirect_to action: "show" if @set_card
+
+  end
+
+  def pay
+    Payjp.api_key =ENV["PAYJP_ACCESS_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+        card: params['payjp-token']
+      )
+      @card = Card.new(user_id: 1, customer_id: customer.id,card_id:customer.default_card)
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action:"pay"
+      end
+    end
+  end
+
+  def delete 
+    if @set_card.blank?
+    else
+      Payjp.api_key =ENV["PAYJP_ACCESS_KEY"]
+      customer = Payjp::Customer.retrieve(@set_card.customer_id)
+      customer.delete
+      @set_card.delete
+    end
+      redirect_to action: "new"
+  end
+
+  def show 
+    if @set_card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key =ENV["PAYJP_ACCESS_KEY"]
+      customer = Payjp::Customer.retrieve(@set_card.customer_id)
+      @default_card_information = customer.cards.retrieve(@set_card.card_id)
+    end
+  end
+
+  private
+
+  def set_card
+    @set_card = Card.where(user_id: 1).first
   end
 end
---
+++ app/views/exhibition/category_children.json.jbuilder
@@ -0,0 +1,3 @@
+json.array! @category_children do |child|
+  json.name child.name
+end
