ca8792272496d8b049508d6ad0e9a8d1ee7a18bb
akkun016 <nakataakito@icloud.com>
2020-06-06 22:10:09 +0900

    商品の削除機能

--- app/controllers/detail_pages_controller.rb
+++ app/controllers/detail_pages_controller.rb
@@ -2,4 +2,13 @@ class DetailPagesController < ApplicationController
   def index
     @commodity_detail = Commodity.find(params[:id])
   end
+
+  def destroy
+    commodity = Commodity.find(params[:id])
+    if commodity.destroy
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 end
