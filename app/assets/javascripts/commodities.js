041a7abe415b724417558b7bc2d16295badcf024
maru1124 <59500225+maru1124@users.noreply.github.com>
2020-07-09 16:17:01 +0900

    Update commodities.js

--- app/assets/javascripts/commodities.js
+++ app/assets/javascripts/commodities.js
@@ -193,25 +193,24 @@ $(document).on('turbolinks:load', function(){
       }
     })
   })
-})
 
-$(function(){
-  $('.text-form__price-input').on('input', function(){   //リアルタイムで表示したいのでinputを使う｡入力の度にイベントが発火するようになる｡
-    var data = $('.text-form__price-input').val(); // val()でフォームのvalueを取得(数値)
-    var profit = Math.round(data * 0.9)  // 手数料計算を行う｡dataにかけているのが0.9なのは単に引きたい手数料が10%のため｡
-    var fee = (data - profit) // 入力した数値から計算結果(profit)を引く｡それが手数料となる｡
-    $('.fee-price').html(fee) //  手数料の表示｡html()は追加ではなく､上書き｡入力値が変わる度に表示も変わるようにする｡
-    $('.fee-price').prepend('¥') // 手数料の前に¥マークを付けたいので
-    $('.profit-price').html(profit)
-    $('.profit-price').prepend('¥')
-    $('#price').val(profit) // 計算結果を格納用フォームに追加｡もし､入力値を追加したいのなら､今回はdataを引数に持たせる｡
-    if(profit == '') {   // もし､計算結果が''なら表示も消す｡
-    $('.profit-price').html('');
-    $('.fee-price').html('');
-    }
+  $(function(){
+    $('.text-form__price-input').on('input', function(){   //リアルタイムで表示したいのでinputを使う｡入力の度にイベントが発火するようになる｡
+      var data = $('.text-form__price-input').val(); // val()でフォームのvalueを取得(数値)
+      var profit = Math.round(data * 0.9)  // 手数料計算を行う｡dataにかけているのが0.9なのは単に引きたい手数料が10%のため｡
+      var fee = (data - profit) // 入力した数値から計算結果(profit)を引く｡それが手数料となる｡
+      $('.fee-price').html(fee) //  手数料の表示｡html()は追加ではなく､上書き｡入力値が変わる度に表示も変わるようにする｡
+      $('.fee-price').prepend('¥') // 手数料の前に¥マークを付けたいので
+      $('.profit-price').html(profit)
+      $('.profit-price').prepend('¥')
+      $('#price').val(profit) // 計算結果を格納用フォームに追加｡もし､入力値を追加したいのなら､今回はdataを引数に持たせる｡
+      if(profit == '') {   // もし､計算結果が''なら表示も消す｡
+      $('.profit-price').html('');
+      $('.fee-price').html('');
+      }
+    })
   })
 })
-
 $(function(){
   $(".alert").fadeOut(5000);
 });
