e7b664307c2c743ac1545de83638dd25544901a2
poke3104 <sevenchild_trial@yahoo.co.jp>
2020-07-11 18:28:36 +0900

    アクション名称、変数の変更

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -1,6 +1,6 @@
 class CommoditiesController < ApplicationController
   before_action :set_item,  only: [:edit, :update, :destroy]
-  before_action :show_image, only: [:edit,:update, :destroy]
+  before_action :set_images, only: [:edit,:update, :destroy]
 
   def index
     @commodities = Commodity.all.order(sales_status_id: "DESC", id: "ASC")
@@ -38,8 +38,8 @@ class CommoditiesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(@commodity.category_id)
-    @child_category = @category.parent
+    @commodity.category
+    @child_category = @commodity.category.parent
     @root_category = @child_category.parent
     @category_root_array = Category.where(ancestry: nil).pluck(:name)
 
@@ -52,10 +52,10 @@ class CommoditiesController < ApplicationController
     @child_array << @child_category.name
     @child_array << @child_category.id
 
-    @category_grandchildren_array = Category.where(ancestry: @category.ancestry).pluck(:name)
+    @category_grandchildren_array = Category.where(ancestry: @commodity.category.ancestry).pluck(:name)
     @grandchild_array = []
-    @grandchild_array << @category.name
-    @grandchild_array << @category.id
+    @grandchild_array << @commodity.name
+    @grandchild_array << @commodity.id
 
   end
 
@@ -99,7 +99,7 @@ class CommoditiesController < ApplicationController
     @commodity = Commodity.find(params[:id])
   end
 
-  def show_image
+  def set_images
     @images = Image.where(commodity_id: params[:id])
   end
   
